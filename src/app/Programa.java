fa2c7517df03e4e9b2635505bcd8e37ad7d11e14
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2024-04-07 14:17:24 -0300

    Solução muito ruim

---
+++ src/app/Programa.java
@@ -0,0 +1,59 @@
+package app;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entidades.Reserva;
+
+public class Programa {
+
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner (System.in);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Número do Quarto: ");
+		int numeroQuarto = sc.nextInt();
+		System.out.print("Data de entrada (dd/MM/yyy): ");
+		Date dataEntrada = sdf.parse(sc.next());
+		System.out.print("Data de saída (dd/MM/yyy): ");
+		Date dataSaida = sdf.parse(sc.next());
+		
+		if(!dataSaida.after(dataEntrada)){
+			System.out.println("Erro ao fazer a reserva: Data de saída menor que a data de entrada");
+		}
+		else {
+			Reserva reserva = new Reserva(numeroQuarto, dataEntrada, dataSaida);
+			System.out.println("Reserva: "+ reserva);
+			
+			System.out.println();
+			
+			System.out.println("Entre os dados para atualização da reserva");
+			System.out.print("Data de entrada (dd/MM/yyy): ");
+			dataEntrada = sdf.parse(sc.next());
+			System.out.print("Data de saída (dd/MM/yyy): ");
+			dataSaida = sdf.parse(sc.next());
+			
+			Date agora = new Date();
+			
+			if(dataEntrada.before(agora)|| dataSaida.before(agora)) {
+				System.out.println("Erro na atualização: Datas devem ser futuras");
+			}
+			else if(!dataSaida.after(dataEntrada)) {
+				System.out.println("Erro ao fazer a reserva: Data de saída menor que a data de entrada");
+			}
+			else {
+						
+			reserva.atualizarReserva(dataEntrada, dataSaida);
+			System.out.println("Reserva: "+ reserva);
+			}
+		}
+		
+		
+		sc.close();
+
+	}
+
+}
---
+++ src/model/entidades/Reserva.java
@@ -0,0 +1,75 @@
+package model.entidades;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reserva {
+	
+	private Integer numeroDoQuarto;
+	private Date dataEntrada;
+	private Date dataSaida;
+	
+	private static SimpleDateFormat sdf =  new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reserva() {
+		
+	}
+	
+	public Reserva(Integer numeroDoQuarto, Date dataEntrada, Date dataSaida) {
+		
+		this.numeroDoQuarto = numeroDoQuarto;
+		this.dataEntrada = dataEntrada;
+		this.dataSaida = dataSaida;
+	}
+
+
+	public Integer getNumeroDoQuarto() {
+		return numeroDoQuarto;
+	}
+
+
+	public void setNumeroDoQuarto(Integer numeroDoQuarto) {
+		this.numeroDoQuarto = numeroDoQuarto;
+	}
+
+
+	public Date getDataEntrada() {
+		return dataEntrada;
+	}
+
+
+	
+
+
+	public Date getDataSaida() {
+		return dataSaida;
+	}
+
+
+	public long duracao() {
+		long dif = dataSaida.getTime()-dataEntrada.getTime();
+		return TimeUnit.DAYS.convert(dif,TimeUnit.MILLISECONDS);
+	}
+	
+	public void atualizarReserva(Date entrada, Date saida) {
+		this.dataEntrada = entrada;
+		this.dataSaida = saida;
+	}
+	
+	@Override
+	public String toString() {
+		return "Quarto "+
+				numeroDoQuarto +
+				", Entrada: "+
+				sdf.format(dataEntrada)+
+				", data de saida: "+
+				sdf.format(dataSaida)+
+				", "+
+				duracao()+
+				" noites";
+	}
+	
+	
+	
+}
